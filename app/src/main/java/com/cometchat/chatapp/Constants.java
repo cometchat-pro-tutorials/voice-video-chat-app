d3102475a1965619ca6179bf1fbb95d546dc3abe
hieptl <hieptl.developer@gmail.com>
2021-10-13 10:10:05 +0700

    update gitignore and Constants.java

---
+++ app/src/main/java/com/cometchat/chatapp/Constants.java
@@ -0,0 +1,10 @@
+package com.cometchat.chatapp;
+
+public interface Constants {
+    String COMETCHAT_APP_ID = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String COMETCHAT_REGION = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String COMETCHAT_AUTH_KEY = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String FIREBASE_REALTIME_DATABASE_URL = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String FIREBASE_EMAIL_KEY = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String FIREBASE_USERS = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+}
