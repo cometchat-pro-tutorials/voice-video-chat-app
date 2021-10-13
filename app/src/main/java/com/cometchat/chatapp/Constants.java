1b42b3719ea0ce6602ee1937a0af2bced3dc46bf
hieptl <hieptl.developer@gmail.com>
2021-10-13 10:26:46 +0700

    Update Constants.java

--- app/src/main/java/com/cometchat/chatapp/Constants.java
+++ app/src/main/java/com/cometchat/chatapp/Constants.java
@@ -5,6 +5,7 @@ public interface Constants {
     String COMETCHAT_REGION = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
     String COMETCHAT_AUTH_KEY = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
     String FIREBASE_REALTIME_DATABASE_URL = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
-    String FIREBASE_EMAIL_KEY = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
-    String FIREBASE_USERS = "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx";
+    String FIREBASE_EMAIL_KEY = "email"; // this is not a secret value, it is just a constant variable that will be accessed from different places of the application.
+    String FIREBASE_USERS = "users"; // this is not a secret value, it is just a constant variable that will be accessed from different places of the application.
+
 }
